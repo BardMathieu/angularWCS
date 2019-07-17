29f9dac94b309661cdab588a387fb3d9401c9c7b
BardMathieu <drabland@gmail.com>
2019-07-17 15:11:05 +0200

    02 angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { FormsModule } from '@angular/forms';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignUpComponent
+    SignUpComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  User = {
+    name : 'Doe',
+    firstName : 'John',
+    age : 25,
+    quote : '',
+    photo : 'https://randomuser.me/api/portraits/lego/2.jpg'
+  };
+  Isvisible = true;
+  constructor() { }
+
+  ngOnInit() {
+  }
+  changehide() {
+    if (this.Isvisible === true){
+      this.Isvisible = false;
+    } else {
+      this.Isvisible = true;
+    }
+
+  }
+}
