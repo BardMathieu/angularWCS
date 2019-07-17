d9dc184f043ae24ca186f379e69df64d6bbe98c4
BardMathieu <drabland@gmail.com>
2019-07-17 17:48:56 +0200

    04 angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import { Routes } from '@angular/router';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import {SignUpComponent} from './sign-up/sign-up.component';
+import {UserComponent} from './user/user.component';
 
 const ROUTES: Routes = [
-  { path: 'signup', component: SignUpComponent },
-  { path: 'user', component: UserProfileComponent },
+  {
+    path: '',
+    redirectTo: '/user',
+    pathMatch: 'full'
+  },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'user-profile', component: UserProfileComponent },
+  { path: 'user', component: UserComponent}
 ];
 
 export { ROUTES };
